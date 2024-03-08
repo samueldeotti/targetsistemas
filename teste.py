82da257d124c7a2e85a1b4e050be3b5ae5cb9ad3
Samuel Deotti <samuel_deotti@hotmail.com>
2024-03-08 11:31:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
 
+def is_in_fibonacci(number): 
+  a, b = 0, 1 while b < number: a, b = b, a + b 
+  return b == number or number == 0
+
+input_number = int(input("Informe um número: "))
+
+if is_in_fibonacci(input_number): 
+  print(f"O número {input_number} pertence à sequência de Fibonacci.")   
+else: 
+  print(f"O número {input_number} não pertence à sequência de Fibonacci.")
