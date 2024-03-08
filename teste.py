b4701613248ae8b93a2e456c66d56f94266a8a72
Samuel Deotti <samuel_deotti@hotmail.com>
2024-03-08 11:32:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-
+#3
 def is_in_fibonacci(number): 
   a, b = 0, 1 while b < number: a, b = b, a + b 
   return b == number or number == 0
@@ -9,3 +9,12 @@ if is_in_fibonacci(input_number):
   print(f"O número {input_number} pertence à sequência de Fibonacci.")   
 else: 
   print(f"O número {input_number} não pertence à sequência de Fibonacci.")
+
+#5
+def reverse_string(string): 
+  return ''.join(string[i] for i in range(len(string) - 1, -1, -1))
+
+string_original = input("Insira uma string: ") 
+string_reversed = reverse_string(string_original) 
+print("String original:", string_original) 
+print("String reversa:", string_reversed)
